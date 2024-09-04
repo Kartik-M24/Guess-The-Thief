0e4fd6b0a1da4f32ae03d410229e8729875fb6e5
Manan <manan9544@gmail.com>
2024-09-04 17:08:21 +1200

    Created a scene manager for all the fxml files

--- src/main/java/nz/ac/auckland/se206/controllers/IntroSceneController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/IntroSceneController.java
@@ -1,11 +1,24 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import nz.ac.auckland.se206.controllers.SceneManager.AppUi;
 
 public class IntroSceneController {
 
   @FXML private ImageView startButton;
 
-  public void changeScene() {}
+  @FXML
+  public void changeScene(MouseEvent event) throws IOException {
+
+    System.out.println("Button clicked: " + startButton.getId());
+    ImageView button = (ImageView) event.getSource();
+    System.out.println("Button clicked: " + button.getId());
+    Scene sceneButtonIsIn = button.getScene();
+
+    sceneButtonIsIn.setRoot(SceneManager.getUiRoot(AppUi.MAINSCENE));
+  }
 }
