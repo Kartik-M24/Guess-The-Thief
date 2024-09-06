c0fefc1d9f72ce52c43e1ab982a8891c54f78454
Kartik-M24 <161120979+Kartik-M24@users.noreply.github.com>
2024-09-06 15:47:14 +1200

    Added the go back to main scene feature on the clues

--- src/main/java/nz/ac/auckland/se206/controllers/FuseBoxClueController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/FuseBoxClueController.java
@@ -1,5 +1,25 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
+
 public class FuseBoxClueController {
-  
+  /**
+   * Handles mouse clicks on rectangles representing people in the room.
+   *
+   * @param event the mouse event triggered by clicking a rectangle
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handleCrimeSceneClick(MouseEvent event) throws IOException {
+    ImageView button = (ImageView) event.getSource();
+    Scene sceneButtonIsIn = button.getScene();
+    sceneButtonIsIn.setRoot(SceneManager.getUiRoot(AppUi.MAINSCENE));
+  }
 }
