c8d296d974369449fa3e5d7b76481d8c14a051df
Manan <manan9544@gmail.com>
2024-09-18 22:12:47 +1200

    Changed so that a few more scene are added in the thread

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -61,6 +61,11 @@ public class App extends Application {
             SceneManager.addUi(SceneManager.AppUi.LETTERCLUE, loadFxml("letterclue"));
             SceneManager.addUi(SceneManager.AppUi.FUSEBOXCLUE, loadFxml("fuseboxclue"));
             SceneManager.addUi(SceneManager.AppUi.LECTERNCLUE, loadFxml("lecternclue"));
+            SceneManager.addUi(
+                SceneManager.AppUi.PHONELOGAUCTIONEER, loadFxml("phonelogauctioneer"));
+            SceneManager.addUi(
+                SceneManager.AppUi.PHONELOGARCHAEOLOGIST, loadFxml("phonelogarchaeologist"));
+            SceneManager.addUi(SceneManager.AppUi.PHONELOGCOLLECTOR, loadFxml("phonelogcollector"));
             AudioManager.addAudio(AudioManager.AudioType.BACKGROUNDMUSIC, "backgroundMusic");
           } catch (IOException e) {
             e.printStackTrace();
@@ -84,9 +89,6 @@ public class App extends Application {
         SceneManager.AppUi.INITIALARTIFACTSCENE, loadFxml("initialscenewithartifact"));
     SceneManager.addUi(
         SceneManager.AppUi.INTIALWITHOUTARTIFACTSCENE, loadFxml("initialscenewithOUTartifact"));
-    SceneManager.addUi(SceneManager.AppUi.PHONELOGAUCTIONEER, loadFxml("phonelogauctioneer"));
-    SceneManager.addUi(SceneManager.AppUi.PHONELOGARCHAEOLOGIST, loadFxml("phonelogarchaeologist"));
-    SceneManager.addUi(SceneManager.AppUi.PHONELOGCOLLECTOR, loadFxml("phonelogcollector"));
     AudioManager.addAudio(AudioManager.AudioType.INITIALTHEFTAUDIO, "intialtheftaudio");
     AudioManager.addAudio(AudioManager.AudioType.AUDIENCEMURMUR, "audienceMurmur");
     scene = new Scene(SceneManager.getUiRoot(AppUi.INTROSCENE));
