eec17776004bad5a11249e899c7a0a14f3683343
Andrew <ajeo563@aucklanduni.ac.nz>
2024-09-18 15:02:43 +1200

    Finished creating the phone log scene for the auctioneer with working rectangle and cursor change on hover.

--- src/main/java/nz/ac/auckland/se206/controllers/PhoneLogAuctioneerController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/PhoneLogAuctioneerController.java
@@ -1,16 +1,41 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
+import nz.ac.auckland.se206.TimerManager;
 
 public class PhoneLogAuctioneerController {
 
   @FXML private ImageView imgSuspects;
+  @FXML private ImageView imgPolice;
+  @FXML private ImageView imgFriend;
+  @FXML private Rectangle rectFriend;
+  @FXML private Rectangle rectPolice;
+  @FXML private Rectangle backButton;
+
+  @FXML private Label timer;
+  private TimerManager timerManager = TimerManager.getInstance();
+
+  /** Initializes the letter clue view. */
+  @FXML
+  public void initialize() {
+    Timeline timeline = TimerManager.getTimeline();
+    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(1), event -> updateTimer()));
+  }
+
+  public void updateTimer() {
+    timer.setText(timerManager.getFormattedTime());
+  }
 
   /**
    * Handles the guess button click event.
@@ -26,6 +51,51 @@ public class PhoneLogAuctioneerController {
   }
 
   /**
+   * Handles the guess button click event.
+   *
+   * @param event the action event triggered by clicking the guess button
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handleFriendClick(MouseEvent event) throws IOException {
+    imgPolice.setOpacity(0);
+    imgFriend.setOpacity(1);
+    backButton.setDisable(false);
+    rectFriend.setDisable(true);
+    rectPolice.setDisable(true);
+  }
+
+  /**
+   * Handles the guess button click event.
+   *
+   * @param event the action event triggered by clicking the guess button
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handlePoliceClick(MouseEvent event) throws IOException {
+    imgPolice.setOpacity(1);
+    imgFriend.setOpacity(0);
+    backButton.setDisable(false);
+    rectFriend.setDisable(true);
+    rectPolice.setDisable(true);
+  }
+
+  /**
+   * Handles the guess button click event.
+   *
+   * @param event the action event triggered by clicking the guess button
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handleBackClick(MouseEvent event) throws IOException {
+    imgPolice.setOpacity(0);
+    imgFriend.setOpacity(0);
+    backButton.setDisable(true);
+    rectFriend.setDisable(false);
+    rectPolice.setDisable(false);
+  }
+
+  /**
    * Handles mouse hover on image
    *
    * @param event the mouse event triggered by clicking a rectangle
@@ -34,5 +104,29 @@ public class PhoneLogAuctioneerController {
   private void onMouseEnteredImage(MouseEvent event) {
     ImageView clickedRectangle = (ImageView) event.getSource();
     clickedRectangle.setCursor(javafx.scene.Cursor.HAND);
+  }
+
+  /**
+   * Handles mouse hover on rectangles to help identify clues
+   *
+   * @param event the mouse event triggered by clicking a rectangle
+   */
+  @FXML
+  private void onMouseEntered(MouseEvent event) {
+    Rectangle clickedRectangle = (Rectangle) event.getSource();
+    clickedRectangle.setCursor(javafx.scene.Cursor.HAND);
+  }
+
+  /**
+   * Handles mouse click to go to the Crime Scene.
+   *
+   * @param event the mouse event triggered by clicking a rectangle
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handleCrimeSceneClick(MouseEvent event) throws IOException {
+    ImageView button = (ImageView) event.getSource();
+    Scene sceneButtonIsIn = button.getScene();
+    sceneButtonIsIn.setRoot(SceneManager.getUiRoot(AppUi.MAINSCENE));
   }
 }
